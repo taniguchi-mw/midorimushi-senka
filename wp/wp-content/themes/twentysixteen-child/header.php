486348ddf036adc71ffbd2ed139fb02f62efce88
taniguchi <taniguchi@media-w.co.jp>
2019-09-27 11:19:13 +0900

    20190927 キャッシュレス消費者還元事業対応※PC(WPも)完了

--- wp/wp-content/themes/twentysixteen-child/header.php
+++ wp/wp-content/themes/twentysixteen-child/header.php
@@ -37,9 +37,9 @@ if(isset($category_description)){
     <?php elseif ( is_category() ) : ?>
     <?php single_cat_title(); ?>
     <?php endif; ?></h1>
-  <div id="logo"><a href="<?php echo home_url( '/' ); ?>" class="over"><img src="<?php echo home_url( '/' ); ?>/img/cmn/logo.jpg" alt="ミドリムシエメラルド公式ショッピングサイトミドリムシ専科" /></a></div>
-  <div id="oparating"><img src="<?php echo home_url( '/' ); ?>/img/cmn/oparating_company.gif" alt="運営会社　ユーコネクト"></div>
-  <div id="h_euglena_midorimushi"><img src="<?php echo home_url( '/' ); ?>/img/cmn/h_ym.gif" alt="ユーグレナ・ミドリムシ"></div>
+  <div id="logo"><a href="<?php echo home_url( '/' ); ?>" class="over"><img src="<?php echo home_url( '/' ); ?>img/cmn/logo.jpg" alt="ミドリムシエメラルド公式ショッピングサイトミドリムシ専科" /></a></div>
+  <div id="oparating"><img src="<?php echo home_url( '/' ); ?>img/cmn/oparating_company.gif" alt="運営会社　ユーコネクト"></div>
+  <div id="h_euglena_midorimushi"><img src="<?php echo home_url( '/' ); ?>img/cmn/h_ym.gif" alt="ユーグレナ・ミドリムシ"></div>
   <div id="mainNavi">
     <ul>
       <li><a href="<?php echo home_url( '/' ); ?>">トップページ</a></li>
@@ -51,24 +51,27 @@ if(isset($category_description)){
       <li><a href="<?php echo home_url( '/' ); ?>flow">ご注文方法</a></li>
     </ul>
   </div>
-  <div id="h_service"><img src="<?php echo home_url( '/' ); ?>/img/cmn/h_service.gif" alt="当店でお買い物をされると･･･全国送料無料、代引手数料無料、最短発送当日、ネット注文でポイント還元" /></div>
+  <div id="h_service"><img src="<?php echo home_url( '/' ); ?>img/cmn/h_service.gif" alt="当店でお買い物をされると･･･全国送料無料、代引手数料無料、最短発送当日、ネット注文でポイント還元" /></div>
   <div id="subNavi">
     <ul>
-      <li><a href="<?php echo home_url( '/' ); ?>order" class="over"><img src="<?php echo home_url( '/' ); ?>/img/cmn/subnv01.jpg" alt="ネットご注文" /></a></li>
-      <li><a href="<?php echo home_url( '/' ); ?>flow" class="over"><img src="<?php echo home_url( '/' ); ?>/img/cmn/subnv02.jpg" alt="FAXでのご注文" /></a></li>
-      <li><a href="https://u-connect.jp/account/my_page_login" target="_blank" class="over"><img src="<?php echo home_url( '/' ); ?>/img/cmn/subnv03.jpg" alt="ログイン" /></a></li>
+      <li><a href="<?php echo home_url( '/' ); ?>order" class="over"><img src="<?php echo home_url( '/' ); ?>img/cmn/subnv01.jpg" alt="ネットご注文" /></a></li>
+      <li><a href="<?php echo home_url( '/' ); ?>flow" class="over"><img src="<?php echo home_url( '/' ); ?>img/cmn/subnv02.jpg" alt="FAXでのご注文" /></a></li>
+      <li><a href="https://u-connect.jp/account/my_page_login" target="_blank" class="over"><img src="<?php echo home_url( '/' ); ?>img/cmn/subnv03.jpg" alt="ログイン" /></a></li>
     </ul>
   </div>
   <div id="h_tel">
     <ul>
-      <li><img src="<?php echo home_url( '/' ); ?>/img/cmn/tel01.gif" alt="当日ご注文：9時半～17時(土日祝日休み)" /></li>
-      <li><img src="<?php echo home_url( '/' ); ?>/img/cmn/tel02.jpg" alt="0120-799-100" /></li>
-      <li><img src="<?php echo home_url( '/' ); ?>/img/cmn/tel03.gif" alt="ミドリムシ専科運営会社 ユーコネクト" /></li>
+      <li><img src="<?php echo home_url( '/' ); ?>img/cmn/tel01.gif" alt="当日ご注文：9時半～17時(土日祝日休み)" /></li>
+      <li><img src="<?php echo home_url( '/' ); ?>img/cmn/tel02.jpg" alt="0120-799-100" /></li>
+      <li><img src="<?php echo home_url( '/' ); ?>img/cmn/tel03.gif" alt="ミドリムシ専科運営会社 ユーコネクト" /></li>
     </ul>
   </div>
   <?php include(dirname(__FILE__) . "/parts/sns.php"); ?>
   <!-- / #header --> 
 </div>
+  <div id="headBnr">
+  	<a href="<?php echo home_url( '/' ); ?>news/index20190927.html"><img src="<?php echo home_url( '/' ); ?>img/campaign/bnr_cashless.png" alt="キャッシュレス・消費者還元事業の対象店です" /></a>
+  </div>
 <div class="breadcrumbs" vocab="https://schema.org/" typeof="BreadcrumbList">
   <?php if(function_exists('bcn_display')){ bcn_display(); }?>
   <!-- //breadcrumb  --> 
